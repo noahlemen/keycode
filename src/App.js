e155299b81e1edd3c01e08e862c8fa5d843f6659
Noah Lemen <noah.lemen@gmail.com>
2017-01-15 21:45:26 -0500

    add react-motion, pretty formatting

--- src/App.js
+++ src/App.js
@@ -1,49 +1,103 @@
 import React, { Component } from 'react';
 import './App.css';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import { TransitionMotion, spring, presets } from 'react-motion';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this);
 
-    this.state = { keyEventStack: [] }
+    this.state = { keyEventStack: [] };
   }
 
   handleKeyDown(event) {
-    const { keyEventStack } = this.state
+    const { keyEventStack } = this.state;
 
     console.log(event);
 
     const newEvent = {
-      key: event.key === ' ' ? 'Space' : event.key,
+      keyName: event.key === ' ' ? 'Space' : event.key,
       keyCode: event.keyCode,
       hash: Math.random().toString(36).substring(7)
-    }
+    };
 
-    this.setState({ keyEventStack: [newEvent, ...keyEventStack.splice(0, 10)] })
+    this.setState({
+      keyEventStack: [ newEvent, ...keyEventStack.splice(0, 10) ]
+    });
 
     event.preventDefault();
   }
 
   componentWillMount() {
-    window.addEventListener('keydown', this.handleKeyDown)
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown)
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  getDefaultStyles() {
+    return this.state.keyEventStack.map(
+      event => ({ ...event, style: { height: 0, opacity: 1 } })
+    );
+  }
+
+  getStyledList() {
+    const { keyEventStack } = this.state;
+    return keyEventStack.map((event, i) => {
+      return {
+        data: { ...event },
+        key: event.hash,
+        style: {
+          height: spring(60, presets.wobbly),
+          opacity: spring(1, presets.stiff)
+        }
+      };
+    });
+  }
+
+  willEnter() {
+    return { height: 0, opacity: 0 };
+  }
+
+  willLeave() {
+    return { height: spring(0), opacity: spring(0) };
   }
 
   render() {
-    const { keyEventStack } = this.state;
-
     return (
       <div className="App">
         <h1>what key code?</h1>
         <h3>press a key to find out</h3>
         <h5>click to copy key code</h5>
-        {keyEventStack.map((event, index) =>  <CopyToClipboard text={event.keyCode}><div className="keyInfo" style={{ opacity: 1.5 / (index + 1) }} key={event.hash}><span className="code">{event.keyCode}</span><span className="key">"{event.key}"</span></div></CopyToClipboard>)}
+        <TransitionMotion
+          defaultStyles={this.getDefaultStyles()}
+          styles={this.getStyledList()}
+          willEnter={this.willEnter}
+          willLeave={this.willLeave}
+        >
+          {styles => (
+              <div>
+                {styles.map(
+                    ({ key, style, data: { keyCode, keyName } }, index) => (
+                      <div style={style} key={key}>
+                        <CopyToClipboard text={keyCode}>
+                          <div
+                            className="keyInfo"
+                            style={{ opacity: 1 / (index + 1) }}
+                          >
+                            <span className="code">{keyCode}</span>
+                            <span className="key">"{keyName}"</span>
+                          </div>
+                        </CopyToClipboard>
+                      </div>
+                    )
+                  )}
+              </div>
+            )}
+        </TransitionMotion>
       </div>
     );
   }
