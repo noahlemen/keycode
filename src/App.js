9ecd535ae75acf5310c2d562a92fe9b57f3f0d85
Noah Lemen <noah.lemen@gmail.com>
2017-01-16 20:30:27 -0500

    github link, custom domain, mobile text, add missing dependencies to package.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { TransitionMotion, spring, presets } from 'react-motion';
 import KeyInfo from './KeyInfo';
 import keycode from 'keycode';
+import isMobile from 'ismobilejs';
 
 class App extends Component {
   constructor(props) {
@@ -88,6 +89,14 @@ class App extends Component {
         <h1>what key code?</h1>
         <h3>press a key to find out</h3>
         <h5>click to copy key code to clipboard</h5>
+        {
+          isMobile.any &&
+            (
+              <h2>
+                this site is probably useless on mobile but go ahead and try if you really want to idc
+              </h2>
+            )
+        }
         <TransitionMotion
           defaultStyles={this.getDefaultStyles()}
           styles={this.getStyledList()}
@@ -117,6 +126,11 @@ class App extends Component {
               </div>
             )}
         </TransitionMotion>
+        <div className="src-link">
+          src on <a href="https://github.com/kedromelon/what-key-code">
+          github
+        </a>
+        </div>
       </div>
     );
   }
